344da29456dc8f57948e11e0a9068122785c4691
TtheNix <nahuelfacundox@gmail.com>
2022-11-18 02:27:46 -0300

    cambios para presentar

--- componentes/script.js
+++ componentes/script.js
@@ -4,8 +4,18 @@ const searchInput = document.getElementById('title')
 const resultContainer = document.getElementById('results-container')
 const form = document.getElementById('form')
 
+const card= document.getElementById("title")
+
+
+/*card.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    response()
+})*/
+
+
 form.addEventListener('submit', async (event) => {
     event.preventDefault()
+    response()
     const body = JSON.stringify({title: searchInput.value})
     let results;
     const API = async () => {
@@ -14,12 +24,31 @@ form.addEventListener('submit', async (event) => {
 
     await API()
     results.forEach(result => {
-        const div = document.createElement('div')
-        div.classList.add('result-card')
-        div.innerHTML = result.title
-        resultContainer.append(div)
+        const container_info = document.querySelector("#results-container")
+        const responseInfo = document.createElement("div");
+        responseInfo.innerHTML = result.title
+
+        container_info.appendChild(responseInfo);
     });
 })
 
+const response = (res) => {
+    clearResponse();
+    const container_res = document.querySelector(".container-res")
+    const responseDiv = document.createElement("div");
+    responseDiv.innerHTML = `
+    <div class="card text-white bg-transparent " style="width: 15rem; height: 20rem;">
+      <img class="cardE" src="../Imagenes/satelites.webp" alt="Card image cap">
+      <div class="card-body">
+      <div id="results-container" class="card-title"></div>
+        <a href="leerMas.html" class="btn btn-primary">Leer Mas</a>
+    </div>`
+    container_res.appendChild(responseDiv);
+}
 
-console.log('pruebaa')
+const clearResponse = () =>{
+    const responses = document.querySelector("#idResponse");
+    while (responses.firstChild) {
+        responses.removeChild(responses.firstChild);
+    }
+}
